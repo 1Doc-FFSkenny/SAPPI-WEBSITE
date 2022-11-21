709f9c61f98fb08f601bb74df9db27a851d0874b
Kenny <47163761+Gaaame@users.noreply.github.com>
2022-11-21 15:49:16 +0800

    11/21/2022
    - Minor text updates

--- sappi-app/src/Components/Contact.js
+++ sappi-app/src/Components/Contact.js
@@ -15,7 +15,7 @@ const Contact = () => {
                             </div>
                             <div class="lg:w-1/2 px-6 mt-4 lg:mt-0">
                                 <h2 class="title-font font-semibold text-gray-900 tracking-widest text-xs">EMAIL</h2>
-                                <a class="text-green-500 leading-relaxed">info@e-agro.ph​</a>
+                                <a class="text-green-500 leading-relaxed">info@sappinc.net​</a>
                                 <h2 class="title-font font-semibold text-gray-900 tracking-widest text-xs mt-4">PHONE</h2>
                                 <p class="leading-relaxed">123-456-7890</p>
                             </div>
